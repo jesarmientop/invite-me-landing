99766e6aa038900ae716bed2e62c4552547939a4
Jeremias Mora <jmora@gisma.cl>
2022-11-02 20:29:03 -0300

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '62ab0a8cd0d87dc273593ea2a351ca1c25cd8cd3',
+        'name' => 'jeremias/inviteme',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jeremias/inviteme' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '62ab0a8cd0d87dc273593ea2a351ca1c25cd8cd3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
