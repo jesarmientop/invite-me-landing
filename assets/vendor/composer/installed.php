8599f3719a644755d3e4df91c7dbf9a0f7e43470
Jeremias Mora <jmora@gisma.cl>
2022-11-03 00:13:01 -0300

    video y estructura assets

---
+++ assets/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '99766e6aa038900ae716bed2e62c4552547939a4',
+        'name' => 'jeremias/inviteme',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jeremias/inviteme' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '99766e6aa038900ae716bed2e62c4552547939a4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
